536a90bf6cdc28f07d0876354f6fdf9741cf0312
BarKabal <bkabala00@gmail.com>
2020-06-03 15:18:20 +0200

    Dodałem 2 testy (GetIfSetup i DisableAll)

--- src/test/java/com/editor/AddLanguagesTest.java
+++ src/test/java/com/editor/AddLanguagesTest.java
@@ -18,4 +18,28 @@ public class AddLanguagesTest extends TestCase {
         Assert.assertFalse(AddLanguages.getIfSetup("html"));
         Assert.assertTrue(AddLanguages.getIfSetup("java"));
     }
+
+    public void testGetIfSetup() {
+        AddLanguages langs = new AddLanguages();
+        AddLanguages.setUpC();
+        AddLanguages.setIfSetup("c",true);
+        AddLanguages.setUpJava();
+        AddLanguages.setIfSetup("java",false);
+
+        Assert.assertFalse(AddLanguages.getIfSetup("java"));
+        Assert.assertTrue(AddLanguages.getIfSetup("c"));
+    }
+
+    public void testDisableAll() {
+        AddLanguages langs = new AddLanguages();
+        AddLanguages.setUpC();
+        AddLanguages.setIfSetup("c",true);
+        AddLanguages.setUpJava();
+        AddLanguages.setIfSetup("java",false);
+
+        AddLanguages.disableAll();
+        Assert.assertFalse(AddLanguages.getIfSetup("c"));
+        Assert.assertFalse(AddLanguages.getIfSetup("html"));
+        Assert.assertFalse(AddLanguages.getIfSetup("java"));
+    }
 }
